e9064a0013942b845b26df594d937d122d78bfad
matveev <matveev@theochem.tu-muenchen.de>
2012-08-17 17:28:30 +0000

    AM: BGY3dH2OData BHD -> State *BHD, 3

--- bgy3dH2OS.h
+++ bgy3dH2OS.h
@@ -1,15 +1,15 @@
-void ReadPairDistribution(BGY3dH2OData BHD, char *filename, Vec g2);
+void ReadPairDistribution (State *BHD, char *filename, Vec g2);
 Vec BGY3dM_solve_H2O_3site(ProblemData *PD, Vec g_ini, int vdim);
 Vec BGY3dM_solve_H2O_2site(ProblemData *PD, Vec g_ini, int vdim);
-void RecomputeInitialFFTs(BGY3dH2OData BHD, real damp, real damp_LJ);
-void RecomputeInitialSoluteData(BGY3dH2OData BHD, real damp, real damp_LJ, real zpad);
-real ComputeCharge(BGY3dH2OData BHD, Vec g1, Vec g2);
-void Compute_H2O_interS(BGY3dH2OData BHD,
+void RecomputeInitialFFTs (State *BHD, real damp, real damp_LJ);
+void RecomputeInitialSoluteData(State *BHD, real damp, real damp_LJ, real zpad);
+real ComputeCharge (State *BHD, Vec g1, Vec g2);
+void Compute_H2O_interS (State *BHD,
                        fftw_complex *(fg2_fft[3]), Vec g, fftw_complex *coul_fft,
                        fftw_complex *(fs_fft[3]), real con, real rho, Vec dg_help);
 #ifdef L_BOUNDARY
-void InitializeLaplaceMatrix(BGY3dH2OData BHD, real zpad);
-void InitializeKSPSolver(BGY3dH2OData BHD);
-real  ImposeLaplaceBoundary(BGY3dH2OData BHD, Vec g, Vec b, Vec x, real zpad, int *iter);
+void InitializeLaplaceMatrix (State *BHD, real zpad);
+void InitializeKSPSolver (State *BHD);
+real  ImposeLaplaceBoundary (State *BHD, Vec g, Vec b, Vec x, real zpad, int *iter);
 #endif
 
