4502b0fc5acef824b31c87cd333c13672461074b
matveev <matveev@theochem.tu-muenchen.de>
2012-08-17 17:27:27 +0000

    AM: BGY3dH2OData BHD -> State *BHD, 2

--- bgy3d-solutes.h
+++ bgy3d-solutes.h
@@ -1,7 +1,7 @@
 /* Solute Functions */
-void RecomputeInitialSoluteData_Methanol(BGY3dH2OData BHD, real damp, real damp_LJ, real zpad);
-void RecomputeInitialSoluteData_Water(BGY3dH2OData BHD, real damp, real damp_LJ, real zpad);
-void RecomputeInitialSoluteData_CS2(BGY3dH2OData BHD, real damp, real damp_LJ, real zpad);
-void RecomputeInitialSoluteData_ButanoicAcid(BGY3dH2OData BHD, real damp, real damp_LJ, real zpad);
-void RecomputeInitialSoluteData_Hexane(BGY3dH2OData BHD, real damp, real damp_LJ, real zpad);
-void RecomputeInitialSoluteData_HCl(BGY3dH2OData BHD, real damp, real damp_LJ, real zpad);
+void RecomputeInitialSoluteData_Methanol (State *BHD, real damp, real damp_LJ, real zpad);
+void RecomputeInitialSoluteData_Water (State *BHD, real damp, real damp_LJ, real zpad);
+void RecomputeInitialSoluteData_CS2 (State *BHD, real damp, real damp_LJ, real zpad);
+void RecomputeInitialSoluteData_ButanoicAcid (State *BHD, real damp, real damp_LJ, real zpad);
+void RecomputeInitialSoluteData_Hexane (State *BHD, real damp, real damp_LJ, real zpad);
+void RecomputeInitialSoluteData_HCl (State *BHD, real damp, real damp_LJ, real zpad);
