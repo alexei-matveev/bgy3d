c536abc49f81e59c1cdb99f5bc46eec17416c10f
matveev <matveev@theochem.tu-muenchen.de>
2012-08-22 17:27:27 +0000

    AM: bgy3d_fft_malloc/free/axpby(), not the best place for them

--- bgy3dmolecule.c
+++ bgy3dmolecule.c
@@ -1450,7 +1450,30 @@ Vec BGY3d_solve_DiatomicAB(ProblemData *PD, Vec g_ini, int vdim)
   return PETSC_NULL;
 }
 
-fftw_complex *ComputeFFTfromVec_fftw(DA da, fftwnd_mpi_plan fft_plan, Vec g,
+/* FIXME:   Maybe  collect  all   of  the   FFT  related   staff  into
+   bgy2d-fft.{c,h}? */
+fftw_complex *bgy3d_fft_malloc (DA da)
+{
+    int x[3], n[3];
+
+    /* Get local portion of the grid */
+    DAGetCorners(da, &(x[0]), &(x[1]), &(x[2]), &(n[0]), &(n[1]), &(n[2]));
+
+    return (fftw_complex*) malloc(n[0] * n[1] * n[2] * sizeof(fftw_complex));
+}
+
+void bgy3d_fft_free (fftw_complex *ptr)
+{
+    free(ptr);
+}
+
+/*
+ * The function  has a feature,  if the output  array g_fft is  NULL a
+ * fresh array is allocated with the size derived from the distributed
+ * array  description.  This  size  is suffucient  to  hold the  local
+ * portion of the array.
+ */
+fftw_complex *ComputeFFTfromVec_fftw (DA da, fftwnd_mpi_plan fft_plan, Vec g,
 				fftw_complex *g_fft, fftw_complex *scratch)
 {
   int index, i[3];
@@ -1460,8 +1483,8 @@ fftw_complex *ComputeFFTfromVec_fftw(DA da, fftwnd_mpi_plan fft_plan, Vec g,
   /* Get local portion of the grid */
   DAGetCorners(da, &(x[0]), &(x[1]), &(x[2]), &(n[0]), &(n[1]), &(n[2]));
 
-  if(g_fft==NULL)
-    g_fft = (fftw_complex*) malloc(n[0]*n[1]*n[2]*sizeof(*g_fft));
+  if (g_fft == NULL)
+      g_fft = (fftw_complex*) malloc(n[0] * n[1] * n[2] * sizeof(*g_fft));
 
   DAVecGetArray(da, g, &g_vec);
   /* loop over local portion of grid */
@@ -1480,7 +1503,6 @@ fftw_complex *ComputeFFTfromVec_fftw(DA da, fftwnd_mpi_plan fft_plan, Vec g,
   fftwnd_mpi( fft_plan, 1, g_fft, scratch, FFTW_NORMAL_ORDER);
 
   return g_fft;
-
 }
 
 
@@ -1519,4 +1541,29 @@ void ComputeVecfromFFT_fftw(DA da, fftwnd_mpi_plan fft_plan, Vec g,
 
 }
 
+/* y := alpha  * x + beta *  y. FIXME: is there anything  like that in
+   FFTW? */
+fftw_complex *bgy3d_fft_axpby (DA da, fftw_complex *restrict y,
+                               real alpha, real beta,
+                               const fftw_complex *x)
+{
+    int z[3], n[3], N;
 
+    /* Get local portion of the grid */
+    DAGetCorners(da, &(z[0]), &(z[1]), &(z[2]), &(n[0]), &(n[1]), &(n[2]));
+
+    N = n[0] * n[1] * n[2];
+
+    if (beta != 0.0)            /* ignore junk in y */
+        for (int i = 0; i < N; i++) {
+            y[i].re = alpha * x[i].re + beta * y[i].re;
+            y[i].im = alpha * x[i].im + beta * y[i].im;
+        }
+    else
+        for (int i = 0; i < N; i++) {
+            y[i].re = alpha * x[i].re;
+            y[i].im = alpha * x[i].im;
+        }
+
+    return y;
+}
--- bgy3dmolecule.h
+++ bgy3dmolecule.h
@@ -8,6 +8,18 @@ void Compute_dg_Pair_normalization(BGY3dDiatomicABData BDD, Vec g1, Vec g2,
 				   Vec dg, Vec dg_help);
 Vec BGY3d_solve_DiatomicAB(ProblemData *PD, Vec g_ini, int vdim);
 void ComputeDiatomicAB_g(Vec g, Vec g0, Vec dg);
+
+
+/* FIXME:   Maybe  collect  all   of  the   FFT  related   staff  into
+   bgy2d-fft.{c,h}? */
+
+fftw_complex *bgy3d_fft_malloc (DA da);
+void bgy3d_fft_free (fftw_complex *ptr);
+
+fftw_complex *bgy3d_fft_axpby (DA da, fftw_complex *restrict y,
+                               real alpha, real beta,
+                               const fftw_complex *x);
+
 fftw_complex *ComputeFFTfromVec_fftw(DA da, fftwnd_mpi_plan fft_plan, Vec g,
 				fftw_complex *g_fft, fftw_complex *work);
 void ComputeVecfromFFT_fftw(DA da, fftwnd_mpi_plan fft_plan, Vec g,
