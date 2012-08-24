dc99616947dbc8ac89104de2967cf3bbcd1a0527
matveev <matveev@theochem.tu-muenchen.de>
2012-08-24 17:17:54 +0000

    AM: pack(), unpack() for real vectors <-> complex arrays

--- bgy3d-fft.c
+++ bgy3d-fft.c
@@ -2,6 +2,7 @@
 /*  $Id: bgy3dmolecule.c,v 1.15 2007-04-23 16:55:13 jager Exp $ */
 /*==========================================================*/
 
+#include <assert.h>
 #include <fftw_mpi.h>
 #include "fft_3d.h"             /* FFT_DATA */
 #include "petscda.h"            /* DA, Vec */
@@ -22,14 +23,7 @@ void bgy3d_fft_free (fftw_complex *ptr)
     free(ptr);
 }
 
-/*
- * The function  has a feature,  if the output  array g_fft is  NULL a
- * fresh array is allocated with the size derived from the distributed
- * array  description.  This  size  is suffucient  to  hold the  local
- * portion of the array.
- */
-fftw_complex *ComputeFFTfromVec_fftw (DA da, fftwnd_mpi_plan fft_plan, Vec g,
-				fftw_complex *g_fft, fftw_complex *scratch)
+static void unpack (DA da, Vec g, fftw_complex *g_fft)
 {
   int index, i[3];
   int x[3], n[3];
@@ -37,9 +31,6 @@ fftw_complex *ComputeFFTfromVec_fftw (DA da, fftwnd_mpi_plan fft_plan, Vec g,
 
   /* Get local portion of the grid */
   DAGetCorners(da, &(x[0]), &(x[1]), &(x[2]), &(n[0]), &(n[1]), &(n[2]));
-
-  if (g_fft == NULL)
-      g_fft = (fftw_complex*) malloc(n[0] * n[1] * n[2] * sizeof(*g_fft));
 
   DAVecGetArray(da, g, &g_vec);
   /* loop over local portion of grid */
@@ -50,19 +41,13 @@ fftw_complex *ComputeFFTfromVec_fftw (DA da, fftwnd_mpi_plan fft_plan, Vec g,
       for(i[0]=x[0]; i[0]<x[0]+n[0]; i[0]++)
 	{
 	  g_fft[index].re = g_vec[i[2]][i[1]][i[0]];
-	  g_fft[index].im = 0;
+	  g_fft[index].im = 0;  /* Vec g is real */
 	  index++;
 	}
   DAVecRestoreArray(da, g, &g_vec);
-  /* forward fft */
-  fftwnd_mpi( fft_plan, 1, g_fft, scratch, FFTW_NORMAL_ORDER);
-
-  return g_fft;
 }
 
-
-void ComputeVecfromFFT_fftw(DA da, fftwnd_mpi_plan fft_plan, Vec g,
-			    fftw_complex *g_fft, fftw_complex *scratch)
+static void pack (DA da, Vec g, const fftw_complex *g_fft)
 {
   int index, i[3];
   int x[3], n[3];
@@ -70,15 +55,6 @@ void ComputeVecfromFFT_fftw(DA da, fftwnd_mpi_plan fft_plan, Vec g,
 
   /* Get local portion of the grid */
   DAGetCorners(da, &(x[0]), &(x[1]), &(x[2]), &(n[0]), &(n[1]), &(n[2]));
-
-  if(g_fft==NULL)
-    {
-      PetscPrintf(PETSC_COMM_WORLD,"Error: g_fft==NULL!\n");
-      exit(1);
-    }
-
-  /* backward fft */
-  fftwnd_mpi( fft_plan, 1, g_fft, scratch, FFTW_NORMAL_ORDER);
 
   DAVecGetArray(da, g, &g_vec);
   /* loop over local portion of grid */
@@ -88,12 +64,47 @@ void ComputeVecfromFFT_fftw(DA da, fftwnd_mpi_plan fft_plan, Vec g,
     for(i[1]=x[1]; i[1]<x[1]+n[1]; i[1]++)
       for(i[0]=x[0]; i[0]<x[0]+n[0]; i[0]++)
 	{
+            /* FIXME: this  is subtle,  we are packing  complex vector
+               into a real array. Imaginary part gets ignored: */
 	  g_vec[i[2]][i[1]][i[0]] = g_fft[index].re;
 	  index++;
 	}
   DAVecRestoreArray(da, g, &g_vec);
+}
+
+/*
+ * The function  has a feature,  if the output  array g_fft is  NULL a
+ * fresh array is allocated with the size derived from the distributed
+ * array  description.  This  size  is suffucient  to  hold the  local
+ * portion of the array.
+ */
+fftw_complex *ComputeFFTfromVec_fftw (DA da, fftwnd_mpi_plan fft_plan, Vec g,
+				fftw_complex *g_fft, fftw_complex *scratch)
+{
+    if (g_fft == NULL)
+        g_fft = bgy3d_fft_malloc (da);
+
+    /* Real Vec into complex array: */
+    unpack (da, g, g_fft);
+
+    /* forward fft */
+    fftwnd_mpi( fft_plan, 1, g_fft, scratch, FFTW_NORMAL_ORDER);
+
+    return g_fft;
+}
 
 
+void ComputeVecfromFFT_fftw(DA da, fftwnd_mpi_plan fft_plan, Vec g,
+			    fftw_complex *g_fft, fftw_complex *scratch)
+{
+    assert (g_fft != NULL);
+
+    /* backward fft */
+    fftwnd_mpi( fft_plan, 1, g_fft, scratch, FFTW_NORMAL_ORDER);
+
+    /* Pack a  complex vector with (hopefully)  vanishing imaginary part
+       into a real Vec: */
+    pack (da, g, g_fft);
 }
 
 /* y := alpha  * x + beta *  y. FIXME: is there anything  like that in
@@ -146,35 +157,21 @@ FFT_DATA *ComputeFFTfromVec(DA da, struct fft_plan_3d *fft_plan, Vec g,
 			    FFT_DATA *g_fft)
 
 {
-  int index=0, i[3];
-  int x[3], n[3];
-  PetscScalar ***g_vec;
+    int x[3], n[3];
 
-  /* Get local portion of the grid */
-  DAGetCorners(da, &(x[0]), &(x[1]), &(x[2]), &(n[0]), &(n[1]), &(n[2]));
+    /* Get local portion of the grid */
+    DAGetCorners(da, &(x[0]), &(x[1]), &(x[2]), &(n[0]), &(n[1]), &(n[2]));
 
-  if(g_fft==NULL)
-    g_fft = (FFT_DATA*) calloc(n[0]*n[1]*n[2],sizeof(*g_fft));
+    if(g_fft==NULL)
+        g_fft = (FFT_DATA*) calloc(n[0] * n[1] * n[2], sizeof(*g_fft));
 
-  DAVecGetArray(da, g, &g_vec);
-  /* loop over local portion of grid */
-  /* Attention: order of indices is not variable */
-  index=0;
-  for(i[2]=x[2]; i[2]<x[2]+n[2]; i[2]++)
-    for(i[1]=x[1]; i[1]<x[1]+n[1]; i[1]++)
-      for(i[0]=x[0]; i[0]<x[0]+n[0]; i[0]++)
-	{
-	  //g_fft[index].re = g_vec[i[2]][i[1]][i[0]]/2.+c;
-	  g_fft[index].re = g_vec[i[2]][i[1]][i[0]];
-	  g_fft[index].im = 0;
-	  index++;
-	}
-  DAVecRestoreArray(da, g, &g_vec);
-  /* forward fft */
-  fft_3d(g_fft, g_fft, 1, fft_plan);
+    /* Real Vec into complex array: */
+    unpack (da, g, g_fft);
 
-  return g_fft;
+    /* forward fft */
+    fft_3d(g_fft, g_fft, 1, fft_plan);
 
+    return g_fft;
 }
 
 
@@ -182,35 +179,12 @@ void ComputeVecfromFFT(DA da, struct fft_plan_3d *fft_plan, Vec g,
 			    FFT_DATA *g_fft)
 
 {
-  int index=0, i[3];
-  int x[3], n[3];
-  PetscScalar ***g_vec;
+    assert (g_fft != NULL);
 
-  /* Get local portion of the grid */
-  DAGetCorners(da, &(x[0]), &(x[1]), &(x[2]), &(n[0]), &(n[1]), &(n[2]));
+    /* backward fft */
+    fft_3d(g_fft, g_fft, -1, fft_plan);
 
-  if(g_fft==NULL)
-    {
-      PetscPrintf(PETSC_COMM_WORLD,"Error: g_fft==NULL!\n");
-      exit(1);
-    }
-
-  /* backward fft */
-  fft_3d(g_fft, g_fft, -1, fft_plan);
-
-  DAVecGetArray(da, g, &g_vec);
-  /* loop over local portion of grid */
-  /* Attention: order of indices is not variable */
-  index=0;
-  for(i[2]=x[2]; i[2]<x[2]+n[2]; i[2]++)
-    for(i[1]=x[1]; i[1]<x[1]+n[1]; i[1]++)
-      for(i[0]=x[0]; i[0]<x[0]+n[0]; i[0]++)
-	{
-	  //g_vec[i[2]][i[1]][i[0]] = g_fft[index].re*2.+c; // Factor 2!!!
-	  g_vec[i[2]][i[1]][i[0]] = g_fft[index].re;
-	  index++;
-	}
-  DAVecRestoreArray(da, g, &g_vec);
-
-
+    /* Pack a  complex vector with (hopefully)  vanishing imaginary part
+       into a real Vec: */
+    pack (da, g, g_fft);
 }
