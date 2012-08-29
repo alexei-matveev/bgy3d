88fe63d3af4d145f36c96732624509baa15850ea
matveev <matveev@theochem.tu-muenchen.de>
2012-08-29 20:02:22 +0000

    AM: this is not used anywhere

--- bgy3d.h
+++ bgy3d.h
@@ -222,24 +222,6 @@ typedef struct BGY3dFourierStruct
 /* diatomic Lennard-Jones */
 
 
-typedef struct BGY3dDiatomicStruct
-{
-  DA da;
-  Vec f[3];
-  Vec v[3];
-
-  real LJ_params[2];            /* sigma and epsilon  */
-  real beta, rho;
-
-  Vec g_ini;
-
-  /* Parallel FFT */
-  struct fft_plan_3d *fft_plan;
-  FFT_DATA *(fg2_fft[3]), *g_fft, *gfg2_fft;
-
-  ProblemData *PD;
-} *BGY3dDiatomicData;
-
 /*******************************************/
 /* Water  */
 /*******************************************/
