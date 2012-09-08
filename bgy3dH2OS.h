4998012078168fa39ae49095f375b7b3b48c7587
matveev <matveev@theochem.tu-muenchen.de>
2012-09-08 22:20:58 +0000

    AM: broken RecomputeInitialSoluteData is used only here

--- bgy3dH2OS.h
+++ bgy3dH2OS.h
@@ -10,7 +10,6 @@ void ReadPairDistribution (const State *BHD, const char *filename, Vec g2);
 Vec BGY3dM_solve_H2O_3site(const ProblemData *PD, Vec g_ini);
 Vec BGY3dM_solve_H2O_2site(const ProblemData *PD, Vec g_ini);
 void RecomputeInitialFFTs (State *BHD, real damp, real damp_LJ);
-void RecomputeInitialSoluteData(State *BHD, real damp, real damp_LJ, real zpad);
 real ComputeCharge (State *BHD, Vec g1, Vec g2);
 void Compute_H2O_interS (const State *BHD,
                          fftw_complex *(fg2_fft[3]), Vec g, real rho, Vec dg_help);
