1dde5f0d87d171aa53a86f0df19fd6dc8159da26
matveev <matveev@theochem.tu-muenchen.de>
2012-10-27 13:15:37 +0000

    AM: comments only

--- bgy3d-poisson.c
+++ bgy3d-poisson.c
@@ -148,14 +148,16 @@ static void InitializeLaplaceMatrix (const DA da, const ProblemData *PD, Mat *M)
 
   const int border = 1 + (int) ceil ((L - 2.0 * zpad) / h[0] / 2.0);
 
-  /* This code constructs (a compact  representation of) the N^3 x N^3
-     matrix  M.  An identity  or another  diagonal matrix  will always
-     have  row == col.   The Laplace  matrix is  sparse as  in "nearly
-     diagonal".  col[3] will be used  to hold the coordinated of three
-     stencil points in a row. Note that "3" is not the space dimension
-     but  rather  the  stencil  size  for  second  derivative  in  one
-     dimension.   The  central  point,   in  col[1]  ==  *(col  +  1),
-     corresponds to the current grid point with indices i[]. */
+  /*
+    This code constructs  (a compact representation of) the  N^3 x N^3
+    matrix M.  An identity or another diagonal matrix will always have
+    row == col.  The Laplace matrix is sparse as in "nearly diagonal".
+    col[3]  will be  used to  hold  the coordinated  of three  stencil
+    points in  a row.  Note that  "3" is not  the space  dimension but
+    rather the  stencil size for  second derivative in  one dimension.
+    The central  point, in  col[1] == *(col  + 1), corresponds  to the
+    current grid point with indices i[].
+  */
 
   /* Get local portion of the grid */
   int x[3], n[3];
