f718c387b425202954b673306d66d23d450e8951
matveev <matveev@theochem.tu-muenchen.de>
2013-01-24 12:55:14 +0000

    AM: make bgy3d_vec_map1/2() static inline

--- bgy3d-vec.h
+++ bgy3d-vec.h
@@ -1,8 +1,5 @@
 /* -*- mode: c; c-basic-offset: 2; -*- vim: set sw=2 tw=70 et sta ai: */
 
-void bgy3d_vec_map1 (Vec ys, real (*f)(real x), Vec xs);
-void bgy3d_vec_map2 (Vec zs, real (*f)(real x, real y), Vec xs, Vec ys);
-
 real bgy3d_vec_mix (Vec dg, Vec dg_new, real a, Vec work);
 
 void bgy3d_vec_save (const char file[], const Vec vec);
@@ -23,4 +20,51 @@ void bgy3d_vec_save_ascii2 (const char *format, int m, /* const */ Vec vec[m][m]
 void bgy3d_vec_read_radial2 (const State *BHD,
                              const char *format, int m, /* const */ Vec g2[m][m]);
 
+static inline int vec_local_size (Vec xs)
+{
+  int n;
+  VecGetLocalSize (xs, &n);
 
+  return n;
+}
+
+/* ys  = map  (f, xs).  Should also  work with  aliased  arguments for
+   in-place transform: */
+static inline void bgy3d_vec_map1 (Vec ys, real (*f)(real x), Vec xs)
+{
+  real *xs_, *ys_;
+
+  VecGetArray (xs, &xs_);
+  VecGetArray (ys, &ys_);
+
+  const int n = vec_local_size (xs);
+  assert (vec_local_size (ys) == n);
+
+  for (int i = 0; i < n; i++)
+    ys_[i] = f (xs_[i]);
+
+  VecRestoreArray (xs, &xs_);
+  VecRestoreArray (ys, &ys_);
+}
+
+/* zs = map (f, xs, ys).   Should also work with aliased arguments for
+   in-place transform: */
+static inline void bgy3d_vec_map2 (Vec zs, real (*f)(real x, real y), Vec xs, Vec ys)
+{
+    real *xs_, *ys_, *zs_;
+
+  VecGetArray (xs, &xs_);
+  VecGetArray (ys, &ys_);
+  VecGetArray (zs, &zs_);
+
+  const int n = vec_local_size (xs);
+  assert (vec_local_size (ys) == n);
+  assert (vec_local_size (zs) == n);
+
+  for (int i = 0; i < n; i++)
+    zs_[i] = f (xs_[i], ys_[i]);
+
+  VecRestoreArray (xs, &xs_);
+  VecRestoreArray (ys, &ys_);
+  VecRestoreArray (zs, &zs_);
+}
