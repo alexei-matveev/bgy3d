2d3b3fae81f041223f6dab586f8b3cc8abf3b7d8
matveev <matveev@theochem.tu-muenchen.de>
2013-05-06 22:28:37 +0000

    AM: rism_dst_many() added

--- rism-dst.c
+++ rism-dst.c
@@ -12,3 +12,19 @@ void rism_dst (size_t n, double out[n], const double in[n])
 
   fftw_destroy_plan (plan);
 }
+
+
+void rism_dst_many (int m, int n, double out[m][n], const double in[m][n])
+{
+  /* FIXME: does it write to in[]?  */
+  const fftw_r2r_kind kind = FFTW_RODFT11;
+
+  fftw_plan plan = fftw_plan_many_r2r (1, &n, m,
+                                       (double*) in, NULL, 1, n,
+                                       (double*) out, NULL, 1, n,
+                                       &kind, FFTW_ESTIMATE);
+
+  fftw_execute (plan);
+
+  fftw_destroy_plan (plan);
+}
