081ce43f69dc797c0be544c0991ac0e6a5ea9422
lib <lib@theochem.tu-muenchen.de>
2013-05-27 16:12:05 +0000

    BL: Get solvent name from command when compling w/o guile

--- hnc3d.c
+++ hnc3d.c
@@ -928,6 +928,9 @@ Vec HNC3d_solvent_solve (const ProblemData *PD, Vec g_ini)
 
   char name[200] = "LJ";        /* solvent & default solute */
 
+  /* Get solvent name or stay with the default: */
+  bgy3d_getopt_string ("--solvent", name, sizeof name);
+
   /* Get the number of solvent sites and their parameters. Get it from
      the solute tables: */
   bgy3d_solute_get (name, &m, &solvent);
@@ -1360,6 +1363,9 @@ Vec HNC3d_solute_solve (const ProblemData *PD, Vec g_ini)
   const Site *solvent;          /* solvent[m] */
 
   char name[200] = "LJ";        /* solvent & default solute */
+
+  /* Get solvent name or stay with the default: */
+  bgy3d_getopt_string ("--solvent", name, sizeof name);
 
   /* Get the number of solvent sites and their parameters. Get it from
      the solute tables: */
