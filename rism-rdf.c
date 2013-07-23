a78c1d3053be831b013d54565c1700441787fbcd
matveev <matveev@theochem.tu-muenchen.de>
2013-07-23 07:15:22 +0000

    AM: add rism-rdf.{c,h} with rism_rdf() function

---
+++ rism-rdf.c
@@ -0,0 +1,53 @@
+/* -*- mode: c; c-basic-offset: 2; -*- vim: set sw=2 tw=70 et sta ai: */
+
+#include "bgy3d.h"              /* State */
+#include "bgy3d-vec.h"          /* vec_create() */
+#include "bgy3d-fftw.h"         /* bgy3d_fft_interp() */
+#include "lebed/lebed.h"        /* genpts() */
+
+void
+rism_rdf (State *dom, Vec g,
+          const real a[3],        /* center */
+          int n, const real r[n], /* radial mesh */
+          int m,                  /* quadrature order */
+          real rdf[n])            /* out */
+{
+  const ProblemData *PD = dom->PD;
+
+  /* Prepare Fourier coefficients: */
+  local Vec g_fft = vec_create (dom->dc);
+
+  MatMult (dom->fft_mat, g, g_fft);
+  /* Do not  VecScale (g_fft, volume_element  (PD)), the interpolation
+     code assumes that ... */
+
+  /* Coordinates  and weights of  spherical quadrature.  Only mm  <= m
+     points are valid: */
+  real x[m][3], w[m];
+  const int mm = genpts (m, x, w);
+
+  /* (Real) grid coordinates and interpolated values: */
+  real y[mm][3], gr[mm];
+
+  /* Over radial layers: */
+  for (int j = 0; j < n; j++)
+    {
+      /* Translate quadrature  coordinates into real  grid coordinates
+         where integer values correspond to the grid nodes: */
+      for (int i = 0; i < mm; i++)
+        FOR_DIM
+          y[i][dim] = (r[j] * x[i][dim] - a[dim] + PD->L[dim] / 2) / PD->h[dim];
+
+      /* FIXME: very expensive trigonometric interpolation: */
+      bgy3d_fft_interp (dom->fft_mat, g_fft, mm, y, gr);
+
+      /* Scale by integration weights: */
+      for (int i = 0; i < mm; i++)
+        gr[i] *= w[i];
+
+      /* Integrate over sphere: */
+      rdf[j] = sum (mm, gr);
+    }
+
+  vec_destroy (&g_fft);
+}
---
+++ rism-rdf.h
@@ -0,0 +1,3 @@
+/* -*- mode: c; c-basic-offset: 2; -*- vim: set sw=2 tw=70 et sta ai: */
+
+void rism_rdf (State *dom, Vec g, const real a[3], int n, const real r[n], int m, real rdf[n]);
