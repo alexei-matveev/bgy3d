7542c19156ae311d2c64953cc3b7a8f5d0969ddd
matveev <matveev@theochem.tu-muenchen.de>
2013-08-02 15:04:38 +0000

    AM: add bessel module with j0() and j1()

--- rism.f90
+++ rism.f90
@@ -532,6 +532,92 @@ module foreign
 end module foreign
 
 
+module bessel
+  !
+  ! One of the many equivalent expresssions:
+  !
+  !               n  / 1  d \ n  sin(x)
+  !   j (x) = (-x)  |  - --- |   ------
+  !    n             \ x dx /      x
+  !
+  ! In particular:
+  !
+  !   j (x) = sin(x) / x
+  !    0
+  !
+  !   j (x) = (sin(x) / x - cos(x)) / x
+  !    1
+  !
+  use kinds, only: rk
+  implicit none
+  private
+
+  public :: j0, j1
+
+contains
+
+  elemental function j0 (x) result (f)
+    !
+    ! sin(x) / x
+    !
+    implicit none
+    real (rk), intent (in) :: x
+    real (rk) :: f
+    ! *** end of interface ***
+
+    real (rk), parameter :: c0 = 1
+    real (rk), parameter :: c1 = -c0 / 6.0d0
+    real (rk), parameter :: c2 =  c0 / 120.0d0
+    real (rk), parameter :: c3 = -c0 / 5040.0d0
+    real (rk), parameter :: c4 =  c0 / 362880.0d0
+    real (rk), parameter :: c5 = -c0 / 39916800.0d0
+    real (rk), parameter :: c6 =  c0 / 6227020800.0d0 ! > 2^32, use doubles
+
+    if (abs (x) < 0.5) then
+      block
+         real (rk) :: y
+
+         y = x * x
+         f =  1 + y * (c1 + y * (c2 + y * (c3 + y * (c4 + y * (c5 + y * c6)))))
+      end block
+    else
+       f = sin (x) / x
+    endif
+  end function j0
+
+
+  elemental function j1 (x) result (f)
+    !
+    ! (sin(x) / x - cos(x)) / x
+    !
+    implicit none
+    real (rk), intent (in) :: x
+    real (rk) :: f
+    ! *** end of interface ***
+
+   real (rk), parameter :: c0 = 1
+   real (rk), parameter :: c1 = -c0 / 10.0d0
+   real (rk), parameter :: c2 =  c0 / 280.0d0
+   real (rk), parameter :: c3 = -c0 / 15120.0d0
+   real (rk), parameter :: c4 =  c0 / 1330560.0d0
+   real (rk), parameter :: c5 = -c0 / 172972800.0d0
+
+   if (abs (x) < 0.25) then
+      block
+         real (rk) :: y, sum
+
+         y = x * x
+         sum = 1 + y * (c1 + y * (c2 + y * (c3 + y * (c4 + y * c5))))
+         f = x / 3 * sum;
+      end block
+   else
+      f = (sin (x) / x - cos (x)) / x
+   endif
+  end function j1
+
+end module bessel
+
+
 module rism
   use kinds, only: rk
   implicit none
