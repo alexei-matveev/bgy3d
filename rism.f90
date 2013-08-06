916f77c6704584ff0ea49f348f0219a463ae4177
matveev <matveev@theochem.tu-muenchen.de>
2013-08-06 09:51:13 +0000

    AM: axes(), dipole_axes(), local_coords() added

--- rism.f90
+++ rism.f90
@@ -1230,6 +1230,36 @@ contains
 
     integer :: nrad, n, m
 
+    block
+       integer :: j
+       integer, parameter :: one(3, 3) = &
+            reshape ([1, 0, 0,  0, 1, 0,  0, 0, 1], [3, 3])
+       real (rk) :: u(3, 3), x(3, size (solvent))
+       type (site) :: sites(size (solvent))
+
+       sites = solvent
+       call show_sites ("Before!", sites, rho)
+       print *, "# Dipole before =", dipole (sites)
+
+       print *, "# Solvent axes:"
+       print *, "# center =", center (sites)
+       u = dipole_axes (sites)
+
+       print *, "# diff =", maxval (abs (matmul (u, transpose (u)) - one))
+       print *, "# diff =", maxval (abs (matmul (transpose (u), u) - one))
+
+       do j = 1, 3
+          print *, "# axis(", j, ") =", u(:, j)
+       enddo
+
+       x = local_coords (sites, u)
+       do j = 1, size (sites)
+          sites(j) % x = x(:, j)
+       enddo
+       call show_sites ("After!", sites, rho)
+       print *, "# Dipole after =", dipole (sites)
+    end block
+
     nrad = size (t, 1)
     n = size (t, 2)
     m = size (t, 3)
@@ -2158,5 +2188,256 @@ contains
        m(i) = sum (sites % charge * sites % x(i))
     enddo
   end function dipole
+
+
+  function weights (sites) result (w)
+    !
+    ! Site weights (volumes) normalized to 1.
+    !
+    use foreign, only: site
+    implicit none
+    type (site), intent (in) :: sites(:)
+    real (rk) :: w(size (sites))
+    ! *** end of interface ***
+
+    ! Choice of the weights as site "volumes":
+    w = (sites % sigma)**3
+
+    w = w / sum (w)
+  end function weights
+
+
+  function center (sites) result (c)
+    !
+    ! We define the center of a  species as a weighted sum of the site
+    ! coordinates with weights proportional  to the "volume" of a site
+    ! (σ³).  A  "center" appears in  the discussion of  the long-range
+    ! site-site correlations for charged sites in a dipole species.  A
+    ! neutral  dipole species  has no  charge center.   A  mass center
+    ! appears  to  be  off-topic   in  the  discussion  of  long-range
+    ! correlations (masses  never appear in the  equations). This kind
+    ! of "geometric" center is the best we can offer here. Suggestions
+    ! are welcome.
+    !
+    use foreign, only: site
+    implicit none
+    type (site), intent (in) :: sites(:)
+    real (rk) :: c(3)
+    ! *** end of interface ***
+
+    integer :: i
+    real (rk) :: w(size (sites))
+
+    ! Choice of the weights:
+    w = weights (sites)
+
+    do i = 1, 3
+       c(i) = sum (w * sites % x(i))
+    enddo
+  end function center
+
+
+  function axes (sites) result (u)
+    use foreign, only: site
+    use linalg, only: eigv
+    use iso_fortran_env, only: error_unit
+    implicit none
+    type (site), intent (in) :: sites(:)
+    real (rk) :: u(3, 3)
+    ! *** end of interface ***
+
+    integer :: s, i, j
+    real (rk) :: w(size (sites))
+    real (rk) :: d(3), c(3), t(3, 3), e(3)
+
+    ! Geometric center of the species:
+    c = center (sites)
+
+    ! The same weights as used in center():
+    w = weights (sites)
+
+    t = 0.0
+    do s = 1, size (sites)
+       ! Site coordinates relative to the center:
+       d = sites(s) % x - c
+
+       do j = 1, 3
+          do i = 1, 3
+             t(i, j) = t(i, j) + w(s) * d(i) * d(j)
+          enddo
+       enddo
+    enddo
+
+    ! u(:, :) will be the eigenvectors, and e(:) will be the
+    ! eigenvalues:
+    call eigv (t, e, u)
+
+    ! FIXME: do  something about degenerate  shapes. The axes  are not
+    ! well defined by  the current procedure in such  cases.  At least
+    ! issue a warning ...
+    if (minval (abs (e(2:) - e(:2))) < 1.0d-7) then
+       write (error_unit, *) " WARNING: degenerate shape, e =", e
+    endif
+  end function axes
+
+
+  function dipole_axes (sites) result (u)
+    use foreign, only: site
+    use linalg, only: eigv
+    use iso_fortran_env, only: error_unit
+    implicit none
+    type (site), intent (in) :: sites(:)
+    real (rk) :: u(3, 3)
+    ! *** end of interface ***
+
+    real (rk) :: m(3)
+
+    ! Z-axis will be collinear with the dipole moment:
+    m = dipole (sites)
+
+    ! FIXME: should be just return axes() in this case?
+    if (norm2 (m) < 1.0d-7) then
+       write (error_unit, *) " WARNING: zero dipole, m =", m
+    endif
+
+    ! m is a unit vector from now on:
+    m = m / norm2 (m)
+
+    !
+    ! The other two axes should be orthogonal to the dipole vector. It
+    ! is somewhat unsettling that  the two remaining axes are somewhat
+    ! arbitrary. This is just one  of the many possible ways to choose
+    ! them:
+    !
+    block
+       integer :: i, j, k, loc(1)
+       real (rk) :: v(3, 3), e(3)
+
+       ! This proposes "shape tensor" eigenvectors as local axes:
+       v = axes (sites)
+
+       ! We are going to replace the axis closest to the dipole vector
+       ! with the dipole  vector. To find such a  vector, first remove
+       ! the collinear component from all three:
+       do j = 1, 3
+          v(:, j) = v(:, j) - m * dot_product (m, v(:, j))
+          e(j) = dot_product (v(:, j), v(:, j))
+       enddo
+
+       ! The one that has the smalles magnitude was the closest to the
+       ! m-axis:
+       loc = minloc (e)
+       i = loc(1)
+
+       ! Reorder vectors and make m the last one:
+       k = 0
+       do j = 1, 3
+          if (j == i) cycle
+          k = k + 1
+          u(:, k) = v(:, j)
+       enddo
+       u(:, 3) = m
+
+       ! Make u(:, 1)  and u(:, 2) orthogonal to  each other (both are
+       ! already orthogonal to m):
+       u(:, 2) = u(:, 2) - u(:, 1) &
+            * dot_product (u(:, 1), u(:, 2)) / dot_product (u(:, 1), u(:, 1))
+
+       ! Make them unit vectors:
+       do j = 1, 3
+          u(:, j) = u(:, j) / norm2 (u(:, j))
+       enddo
+    end block
+
+    ! This is another choice of the two remaining vectors:
+    block
+       integer :: s, i, j
+       real (rk) :: w(size (sites))
+       real (rk) :: c(3), d(3), dxy(2), t(2, 2), v(2, 2), e(2)
+
+       ! Geometric center of the species:
+       c = center (sites)
+
+       ! Choice of the weights:
+       w = weights (sites)
+
+       t = 0.0
+       do s = 1, size (sites)
+          ! Site coordinates relative to the center:
+          d = sites(s) % x - c
+
+          ! x- and y-coordinates given the two axes orthogonal to m:
+          do j = 1, 2
+             dxy(j) = dot_product (d, u(:, j))
+          enddo
+
+          !
+          ! Increment the  2x2 "shape thensor". This is  the same kind
+          ! of shape tensor we used to propose the default (not dipole
+          ! related) axes, albeit restricted to two dimensions:
+          !
+          do j = 1, 2
+             do i = 1, 2
+                t(i, j) = t(i, j) + w(s) * dxy(i) * dxy(j)
+             enddo
+          enddo
+       enddo
+
+       !
+       ! Here the 2x2 eigenvalue problem is being solved: v(:, :) will
+       ! be  the   eigenvectors,  and   e(:)  will  be   the  (unused)
+       ! eigenvalues:
+       !
+       call eigv (t, e, v)
+
+       !
+       ! Alternative x- and y-axes are just another linear combination
+       ! of the old ones (did I mention they are somewhat arbitrary?):
+       !
+       ! u   := Σ  u   v
+       !  ij     k  ik  kj
+       !
+       u(1:3, 1:2) = matmul (u(1:3, 1:2), v(1:2, 1:2))
+
+       ! FIXME: Still, if say in water, the hydrogens have zero radius
+       ! (weights), they do not  contribute to the "shape tensor" (the
+       ! species is  effectively a cylinder  or rather a ball  in this
+       ! case) and thus  x- and y- axes are  still arbitrary as coming
+       ! out of  the eigensolver.  So far  we just issue  a warning in
+       ! such cases:
+       if (abs (e(2) - e(1)) < 1.0d-7) then
+          write (error_unit, *) " WARNING: degenerate shape, e =", e
+       endif
+    end block
+  end function dipole_axes
+
+
+  function local_coords (sites, u) result (x)
+    !
+    ! Returns  orientation   independent  coordinates  of   the  sites
+    ! relative to the "center" of the species.
+    !
+    ! If supplied  the axes u(:,  :) as prepared by  dipole_axes() the
+    ! Z-axis is collinear with the dipole moment by construction.
+    !
+    use foreign, only: site
+    implicit none
+    type (site), intent (in) :: sites(:)
+    real (rk), intent (in) :: u(3, 3) ! orthogonal matrix
+    real (rk) :: x(3, size (sites))
+    ! *** end of interface ***
+
+    integer :: i, j
+    real (rk) :: c(3)
+
+    c = center (sites)
+
+    do j = 1, size (sites)
+       do i = 1, 3
+          x(i, j) = dot_product (u(:, i), sites(j) % x - c)
+       enddo
+    enddo
+  end function local_coords
+
 
 end module rism
