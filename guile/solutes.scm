2145f216b085cd9e8508c5e90702cfbacd27cdc0
lib <lib@theochem.tu-muenchen.de>
2013-10-01 12:53:25 +0000

    BL: add new uranyl FF (PM13)

--- guile/solutes.scm
+++ guile/solutes.scm
@@ -543,10 +543,23 @@
   ("O" (-1.76 0.0 0.0) 2.8494 0.4380613581 -0.25)
   ("O" (1.76 0.0 0.0) 2.8494 0.4380613581 -0.25)))
 
-("uranyl, TIP5P"
- (("U" (0.0 0.0 0.0) 3.26 0.26 2.5)
-  ("O" (-1.76 0.0 0.0) 2.92 1.47 -0.25)
-  ("O" (1.76 0.0 0.0) 2.92 1.47 -0.25)))
+;;;
+;;; Another set of force field which is also developed by E. J. Maginn
+;;; et.al., see [1] for more details. Explicit parameters could be found
+;;; in the Gromac input file provided by the author:
+;;; http://www.rsc.org/suppdata/cp/c3/c3cp52444b/c3cp52444b_2.pdf
+;;;
+;;; [1] Development and application of effective pairwise potentials for
+;;;     UO2n+, NpO2n+, PuO2n+, and AmO2n+ (n = 1, 2) ions with water,
+;;;     Vladimir Pomogaev, Surya Prakash Tiwari, Neeraj Rai,
+;;;     George S. Goff, Wolfgang Runde, William F. Schneider and
+;;;     Edward J. Maginn, Phys. Chem. Chem. Phys., 2013, Advance Article
+;;;     http://dx.doi.org/10.1039/C3CP52444B
+;;;
+("uranyl, PM13" ;;; FIXME: need better name
+ (("U" (0.0 0.0 0.0) (A 2.95) (kJ 0.530) 2.5)
+  ("O" (-1.761 0.0 0.0) (A 3.85) (kJ 0.057) -0.25)
+  ("O" (1.761 0.0 0.0) (A 3.85) (kJ 0.057) -0.25)))
 
 ;;;
 ;;; This  is a fake  solute which  tabulates some  half-way meaningful
