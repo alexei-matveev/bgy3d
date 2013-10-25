16ac94ef61e621b5b9a37592d08158983ba47c9a
matveev <matveev@theochem.tu-muenchen.de>
2013-10-25 09:32:07 +0000

    AM: uranyl with 5 waters added

---
+++ test-qm/uranyl/run2.py
@@ -0,0 +1,73 @@
+#
+# Tell Python interpreter where to find custom modules:
+#
+#   export PYTHONPATH=~/PYTHON:~/darcs/bgy3d/python
+#
+# For the  rubgy.scm script to run  one might need to  set the library
+# search path:
+#
+# export LD_LIBRARY_PATH=~/darcs/bgy3d
+#
+from ase.calculators.paragauss import ParaGauss
+from ase.io import read, write
+from pts.memoize import Memoize, DirStore
+from pts.units import kcal
+from pts.func import compose, NumDiff
+from pts.zmat import Fixed, Rigid, ManyBody
+from pts.qfunc import QFunc
+from pts.fopt import minimize
+from rism import Server
+from numpy import max, abs
+
+cmd = \
+"""
+/home/alexei/darcs/bgy3d/guile/runbgy.scm
+--solute "UO2_5H2O, SPC"
+"""
+#--norm-tol 1e-14 --dielectric 78.4
+#--rho 0.0333295 --beta 1.6889 --L 160 --N 4096
+#--solute "uranyl, SPC"
+#--solvent "water, SPC/E"
+
+calc = ParaGauss (cmdline="mpirun --np 4 ~/darcs/ttfs-mac/runqm",
+                  input="uranyl+water.scm")
+
+atoms = read ("uo22+,5h2o.xyz")
+
+x = atoms.get_positions ()
+
+xs = [x[3 * i: 3 * i + 3] for i in range (len (x) / 3)]
+fs = [Rigid (y) for y in xs]
+trafo = ManyBody (Fixed (xs[0]), *fs[1:])
+s = trafo.pinv (x) # s == zeros (6 * len (fs) - 6)
+
+assert max (abs (s)) == 0.0
+assert max (abs (trafo (s) - x)) < 1.0e-10
+
+with QFunc (atoms, calc) as f, Server (cmd) as g:
+    f = Memoize (f, DirStore (salt="uo22+, 5h2o, qm"))
+    f = compose (f, trafo)
+    g1 = Memoize (g, DirStore (salt="uo22+, rism"))
+    g = lambda x: g1(x) * kcal
+    g = compose (g, trafo)
+    g = NumDiff (g)
+
+    e = f # + g
+
+    print "s(start)=", s
+    print "x(start)=\n", trafo (s)
+    print "e(start)=", e (s)  / kcal
+
+    s, info = minimize (e, s, algo=1, maxit=50, ftol=1.0e-2, xtol=1.0e-2)
+
+    print "converged =", info["converged"], "in", info["iterations"], "iterations"
+    print "s(min)=", s
+    print "x(min)=\n", trafo (s)
+    atoms.set_positions (trafo (s))
+    write ("a50.xyz", atoms)
+
+    # units = [(kcal, "kcal"), (eV, "eV"), (Hartree, "Hartree")]
+    # for u, uu in units:
+    #     print "e = ", f(s)/u, "+", g(s)/u, "=", e(s)/u, "(%s)" % uu
+    # for u, uu in units:
+    #     print "g = ", f.fprime(s)/u, "+", g.fprime(s)/u, "=", e.fprime(s)/u, "(%s/Unit)" % uu
---
+++ test-qm/uranyl/uranyl+water.scm
@@ -0,0 +1,55 @@
+;;
+;; UO22+, RKS.
+;;
+((operations
+  (operations-symm #t)
+  (operations-integral #t)
+  (operations-scf #t)
+  (operations-geo-opt #t)
+  (operations-dipole #t)
+  (operations-properties #f)
+  (operations-solvation-effect #f))
+ (main-options
+  (integrals-on-file #f)                ; This is faster
+  (relativistic "true")                 ; This is an AE calculation
+  (spin-restricted #t))                 ; UO22+ is closed shell
+ (geo
+  (units angstrom)
+  ("U"  (-0.4193  0.2123  0.1018))
+  ("OU"  (1.1822  0.9792  0.1743))
+  ("OU"  (-2.0068  -0.5497  -0.1304))
+  ("OW"  (-1.6172  2.2191  -0.7126))
+  ("HW"  (-2.5526  2.2260  -0.9995))
+  ("HW"  (-1.2731  3.1280  -0.8244))
+  ("OW"  (0.9046  -1.8307  -0.3086))
+  ("HW"  (0.5819  -2.7447  -0.4413))
+  ("HW"  (1.8777  -1.8501  -0.4076))
+  ("OW"  (-0.1489  0.1448  -2.3461))
+  ("HW"  (-0.8081  -0.1703  -2.9968))
+  ("HW"  (0.6309  0.4518  -2.8510))
+  ("OW"  (-0.6837  1.7241  2.0738))
+  ("HW"  (-1.4562  1.8695  2.6561))
+  ("HW"  (0.0204  2.3354  2.3698))
+  ("OW"  (-0.6191  -1.1831  2.1599))
+  ("HW"  (-1.3870  -1.7658  2.3269))
+  ("HW"  (-0.0073  -1.2855  2.9163)))
+ (~ "U_24.19.16.11_10.7.7.4")
+ (~ "O_9.5.1_5.4.1")
+ (~ "O_9.5.1_5.4.1")
+ (rep 5
+      (ecp "nwchem" "O" "crenbl_ecp" "ahlrichs_coulomb_fitting")
+      (bas "nwchem" "H" "crenbl_ecp" "ahlrichs_coulomb_fitting")
+      (bas "nwchem" "H" "crenbl_ecp" "ahlrichs_coulomb_fitting"))
+ (mixing (chmix 0.1) (start-after-cycle 5))
+ (grid (sym-reduce #t) (weight-grads #t))
+ (rep 18 (gridatom (nrad 50) (nang 291)))
+ (xc-control (xc "pbe"))
+ (occupation (charge 2.0))
+ (convergence-list
+  (max-geo-iteration 0))
+ (diis (diis-on #f))
+ (solvation
+  (smoothing "fixpva")
+  (sol-start-cycle 10)
+  (correction-type "None")))
+
