bd50a36745d4f0a38de1fad0791070f5ba25f12b
matveev <matveev@theochem.tu-muenchen.de>
2013-11-25 17:02:14 +0000

    AM: car, cdr, assoc, not, bool added

--- lisp.f90
+++ lisp.f90
@@ -10,7 +10,39 @@ module lisp
   end type obj
 
   ! FIXME: this is for Guile 2.0:
-  type (obj), parameter :: nil = obj (z"304")
+  type (obj), parameter :: false = obj (z"004")       ! #f
+  type (obj), parameter :: nil = obj (z"304")         ! ()
+  type (obj), parameter :: true = obj (z"404")        ! #t
+  type (obj), parameter :: unspecified = obj (z"804") ! *unspecified*
+  type (obj), parameter :: undefined = obj (z"904")   ! for SCM_UNBNDP()
+  type (obj), parameter :: eof = obj (z"a04")         ! for eof-object?
+
+  interface car
+     function scm_car (pair) result (car) bind (c)
+       import
+       implicit none
+       type (obj), intent (in), value :: pair
+       type (obj) :: car
+     end function scm_car
+  end interface car
+
+  interface cdr
+     function scm_cdr (pair) result (cdr) bind (c)
+       import
+       implicit none
+       type (obj), intent (in), value :: pair
+       type (obj) :: cdr
+     end function scm_cdr
+  end interface cdr
+
+  interface assoc
+     function scm_assoc (key, alist) result (pair) bind (c)
+       import
+       implicit none
+       type (obj), intent (in), value :: key, alist
+       type (obj) :: pair
+     end function scm_assoc
+  end interface assoc
 
   interface cons
      function scm_cons (car, cdr) result (pair) bind (c)
@@ -60,6 +92,11 @@ module lisp
      end function scm_to_int32
   end interface int
 
+  interface bool
+     module procedure from_bool
+     module procedure to_bool
+  end interface bool
+
   interface
      function scm_from_locale_symboln (str, len) result (symbol) bind (c)
        import
@@ -76,14 +113,77 @@ module lisp
        integer (c_size_t), intent (in), value :: len
        type (obj) :: string
      end function scm_from_locale_stringn
+
+     function scm_to_bool (bool) result (int) bind (c)
+       import
+       implicit none
+       type (obj), intent (in), value :: bool
+       integer (c_int) :: int
+     end function scm_to_bool
+
+     function scm_display (object, port) result (undef) bind (c)
+       import
+       implicit none
+       type (obj), intent (in), value :: object, port
+       type (obj) :: undef
+     end function scm_display
+
+     function scm_newline (port) result (undef) bind (c)
+       import
+       implicit none
+       type (obj), intent (in), value :: port
+       type (obj) :: undef
+     end function scm_newline
   end interface
 
-  public :: obj                        ! type
-  public :: float, int, symbol, string ! constructors
+  interface not
+     function scm_not (object) result (bool) bind (c)
+       import
+       implicit none
+       type (obj), intent (in), value :: object
+       type (obj) :: bool
+     end function scm_not
+  end interface
+
+  public :: obj                              ! type
+  public :: float, int, symbol, string, bool ! constructors
   public :: cons, nil
-  public :: acons
+  public :: car, cdr
+  public :: acons, assoc, not
+  public :: display, newline
 
 contains
+
+  subroutine display (object, port)
+    implicit none
+    type (obj), intent (in) :: object, port
+    optional :: port
+    ! *** end of interface **
+
+    type (obj) :: ignore
+
+    if (present (port)) then
+       ignore = scm_display (object, port)
+    else
+       ignore = scm_display (object, undefined)
+    endif
+  end subroutine display
+
+
+  subroutine newline (port)
+    implicit none
+    type (obj), intent (in), optional :: port
+    ! *** end of interface **
+
+    type (obj) :: ignore
+
+    if (present (port)) then
+       ignore = scm_newline (port)
+    else
+       ignore = scm_newline (undefined)
+    endif
+  end subroutine newline
+
 
   function symbol (fstr) result (symb)
     implicit none
@@ -97,6 +197,7 @@ contains
     symb = scm_from_locale_symboln (fstr, slen)
   end function symbol
 
+
   function string (fstr) result (lstr)
     implicit none
     character (len=*), intent (in) :: fstr
@@ -108,6 +209,32 @@ contains
     slen = len (fstr)
     lstr = scm_from_locale_stringn (fstr, slen)
   end function string
+
+
+  function from_bool (flag) result (bool)
+    implicit none
+    logical, intent (in) :: flag
+    type (obj) :: bool
+    ! *** end of interface **
+
+    ! SCM scm_from_bool(int) is a C-macro!
+    if (flag) then
+       bool = true
+    else
+       bool = false
+    endif
+  end function from_bool
+
+
+  function to_bool (bool) result (flag)
+    implicit none
+    type (obj), intent (in) :: bool
+    logical :: flag
+    ! *** end of interface **
+
+    flag = (scm_to_bool (bool) /= 0)
+  end function to_bool
+
 
   function acons (key, val, old) result (new)
     !
