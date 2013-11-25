fcfa97b8d12475c79dfa9c4a968661685c0e49c4
matveev <matveev@theochem.tu-muenchen.de>
2013-11-25 20:07:54 +0000

    AM: geopt(alist, key, val) to extract fortran values

--- options.f90
+++ options.f90
@@ -4,6 +4,11 @@ module options
   private
 
   interface getopt
+     module procedure getenv_test
+     module procedure getenv_bool
+     module procedure getenv_real
+     module procedure getenv_int
+
      module procedure getopt_test
      module procedure getopt_int
      module procedure getopt_real
@@ -14,6 +19,79 @@ module options
 
 contains
 
+  !
+  ! These are  used to inquire settings organized  into an association
+  ! list supplied as the first argument:
+  !
+  function getenv_test (env, key) result (ok)
+    use lisp, only: obj, assoc, symbol, not, bool
+    implicit none
+    type (obj), intent (in) :: env
+    character (len=*), intent (in) :: key
+    logical :: ok
+    ! *** end of interface ***
+
+    type (obj) :: pair
+
+    ! "not not" converts any value to true or false:
+    pair = assoc (symbol (key), env)
+    ok = .not. bool (not (pair))
+  end function getenv_test
+
+
+  function getenv_bool (env, key, val) result (ok)
+    use lisp, only: obj, assoc, symbol, not, bool, cdr
+    implicit none
+    type (obj), intent (in) :: env
+    character (len=*), intent (in) :: key
+    logical, intent (inout) :: val
+    logical :: ok
+    ! *** end of interface ***
+
+    type (obj) :: pair
+
+    pair = assoc (symbol (key), env)
+    ok = .not. bool (not (pair))
+    if (ok) val = bool (cdr (pair))
+  end function getenv_bool
+
+
+  function getenv_real (env, key, val) result (ok)
+    use lisp, only: obj, assoc, symbol, not, bool, cdr, float
+    implicit none
+    type (obj), intent (in) :: env
+    character (len=*), intent (in) :: key
+    real (rk), intent (inout) :: val
+    logical :: ok
+    ! *** end of interface ***
+
+    type (obj) :: pair
+
+    pair = assoc (symbol (key), env)
+    ok = .not. bool (not (pair))
+    if (ok) val = float (cdr (pair))
+  end function getenv_real
+
+
+  function getenv_int (env, key, val) result (ok)
+    use lisp, only: obj, assoc, symbol, not, bool, cdr, int
+    implicit none
+    type (obj), intent (in) :: env
+    character (len=*), intent (in) :: key
+    integer, intent (inout) :: val
+    logical :: ok
+    ! *** end of interface ***
+
+    type (obj) :: pair
+
+    pair = assoc (symbol (key), env)
+    ok = .not. bool (not (pair))
+    if (ok) val = int (cdr (pair))
+  end function getenv_int
+
+  !
+  ! These are to inquire PETSC environment:
+  !
   function op (key) result (str)
     use iso_c_binding, only: C_NULL_CHAR
     implicit none
