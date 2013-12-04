5a61bf6403432656412edde769685cb0bd0b6fc7
matveev <matveev@theochem.tu-muenchen.de>
2013-12-04 13:27:40 +0000

    AM: geo opt with MM self-energy

--- test-qm/uranyl/run2.py
+++ test-qm/uranyl/run2.py
@@ -13,38 +13,83 @@ import os
 from ase.calculators.paragauss import ParaGauss
 from ase.io import read, write
 from pts.memoize import Memoize, DirStore
-from pts.units import kcal
+from pts.units import kcal, eV, Hartree
 from pts.func import compose, NumDiff
 from pts.zmat import Fixed, Rigid, ManyBody
 from pts.qfunc import QFunc
 from pts.fopt import minimize
 from rism import Server
-from numpy import max, abs
+from numpy import max, abs, zeros
 
+# With this command  line the RISM server will  return the "gas phase"
+# or self-energy of the solute:
 cmd = \
 """
 /home/alexei/darcs/bgy3d/guile/runbgy.scm
 --solute "UO2_5H2O, SPC"
 """
-#--norm-tol 1e-14 --dielectric 78.4
-#--rho 0.0333295 --beta 1.6889 --L 160 --N 4096
-#--solute "uranyl, SPC"
-#--solvent "water, SPC/E"
+
+# With  thin command  line the  RISM  server will  return the  "excess
+# chemical  potential" of  the  solute in  solvent.  This is,  roughly
+# speaking, the "averaged" solute-solvent interaction:
+alt = \
+"""
+/home/alexei/darcs/bgy3d/guile/runbgy.scm
+--solvent "water, SPC/E"
+--solute "UO2_5H2O, SPC"
+--norm-tol 1e-14 --dielectric 78.4
+--rho 0.0333295 --beta 1.6889 --L 20 --N 512
+"""
 
 calc = ParaGauss (cmdline="mpirun --np 4 ~/darcs/ttfs-mac/runqm",
                   input="uranyl+water.scm")
 
 atoms = read ("uo22+,5h2o.xyz")
+# atoms = read ("a50.xyz")
 
 x = atoms.get_positions ()
 
 xs = [x[3 * i: 3 * i + 3] for i in range (len (x) / 3)]
-fs = [Rigid (y) for y in xs]
-trafo = ManyBody (Fixed (xs[0]), *fs[1:])
-s = trafo.pinv (x) # s == zeros (6 * len (fs) - 6)
 
-assert max (abs (s)) == 0.0
-assert max (abs (trafo (s) - x)) < 1.0e-10
+def make_uranyl (x):
+    """
+    Return the "best" linear approximation to the uranyl geometry as a
+    Fixed() func.
+    """
+    from pts.zmat import ZMat
+    from numpy import pi
+
+    # Make uranyl linear:
+    zmt = ZMat ([(None, None, None),
+                 (1, None, None),
+                 (1, 2, None)], base=1)
+
+    # Bond length and the bond angle for uranyl, use the same Z-matrix
+    # for uranyl:
+    s = zmt.pinv (x)
+
+    # Average bond length and 180 degrees angle. Note that if you plug
+    # these internal variables into z-matrix you will not get zmt(s) ~
+    # x  even though  s was  derived from  x. That  is because  of the
+    # default orientation z-matrix chooses:
+    s_bond = sum (s[:2]) / 2
+    s = [s_bond, s_bond, pi]
+
+    # Uranyl  will be  fixed, but  to adjust  orientation, rotate  a rigid
+    # object:
+    Y = Rigid (zmt (s))
+    return Fixed (Y (Y.pinv (x)))
+
+if True:
+    uranyl = make_uranyl (xs[0]) # linear
+else:
+    uranyl = Fixed (xs[0])      # may be bent
+
+waters = [Rigid (y) for y in xs[1:]]
+trafo = ManyBody (uranyl, *waters)
+
+# 6 dof per rigid water:
+s = zeros (6 * len (waters))
 
 #
 # FIXME: PG will  choke on gxfile, and saved_scfstate  from a previous
@@ -61,30 +106,41 @@ def clean ():
 clean ()
 
 
-with QFunc (atoms, calc) as f:
-    with Server (cmd) as g:
-        f = Memoize (f, DirStore (salt="uo22+, 5h2o, qm"))
-        g = Memoize (g, DirStore (salt=cmd))
+with QFunc (atoms, calc) as f, Server (cmd) as g, Server (alt) as h:
+    # Change  the  salts  to   discard  memoized  results  (or  delete
+    # ./cache.d):
+    f = Memoize (f, DirStore (salt="uo22+, 5h2o, qm"))
+    g = Memoize (g, DirStore (salt=cmd + "Dec3(b)"))
+    h = Memoize (h, DirStore (salt=alt + "Dec3(b)"))
 
-        f = compose (f, trafo)
-        g = compose (g, trafo)
+    f = compose (f, trafo)  # QM self-energy
+    g = compose (g, trafo)  # MM self-energy
+    h = compose (h, trafo)  # RISM solvation energy
 
-        e = f # + g
+    e = g + h # + f
 
-        print "s(start)=", s
-        print "x(start)=\n", trafo (s)
-        print "e(start)=", e (s)  / kcal
+    print "s(start)=", s
+    print "x(start)=\n", trafo (s)
+    print "e(start)=", e (s) / kcal, "g(start)=", g (s) / kcal, "h(start)=", h (s) / kcal
 
-        s, info = minimize (e, s, algo=1, maxit=50, ftol=1.0e-2, xtol=1.0e-2)
+    s, info = minimize (e, s, algo=1, maxstep=0.1, maxit=100, ftol=1.0e-2, xtol=1.0e-2)
 
-        print "converged =", info["converged"], "in", info["iterations"], "iterations"
-        print "s(min)=", s
-        print "x(min)=\n", trafo (s)
-        atoms.set_positions (trafo (s))
-        write ("a50.xyz", atoms)
+    print "converged =", info["converged"], "in", info["iterations"], "iterations"
+    print "s(min)=", s
+    print "x(min)=\n", trafo (s)
+    atoms.set_positions (trafo (s))
+    write ("a50.xyz", atoms)
 
-        # units = [(kcal, "kcal"), (eV, "eV"), (Hartree, "Hartree")]
-        # for u, uu in units:
-        #     print "e = ", f(s)/u, "+", g(s)/u, "=", e(s)/u, "(%s)" % uu
-        # for u, uu in units:
-        #     print "g = ", f.fprime(s)/u, "+", g.fprime(s)/u, "=", e.fprime(s)/u, "(%s/Unit)" % uu
+    units = [(kcal, "kcal"), (eV, "eV"), (Hartree, "Hartree")]
+    for u, uu in units:
+        print "e = ", g(s)/u, "+", h(s)/u, "=", e(s)/u, "(%s)" % uu
+
+    for u, uu in units:
+        print "e = ", e(s) / u, "%s," % uu, \
+            "|g| = ", max(abs(e.fprime(s))) / u, "%s/Unit" % uu
+
+
+    e = f + h
+    for u, uu in units:
+        print "e = ", e(s) / u, "%s," % uu, \
+            "|g| = ", max(abs(e.fprime(s))) / u, "%s/Unit" % uu
