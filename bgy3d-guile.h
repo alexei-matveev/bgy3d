f6f8e21a04c963357003417ba3402d50b7fd4be7
alexei.matveev <alexei.matveev@gmail.com>
2014-04-08 09:06:03 +0000

    AM: update copyright year in ./bgy3d-guile.{c,h}

--- bgy3d-guile.h
+++ bgy3d-guile.h
@@ -1,6 +1,6 @@
 /* -*- mode: c; c-basic-offset: 2; -*- vim: set sw=2 tw=70 et sta ai: */
 /*
-  Copyright (c) 2013 Alexei Matveev
+  Copyright (c) 2013, 2014 Alexei Matveev
   Copyright (c) 2013 Bo Li
 */
 
