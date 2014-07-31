85184702374fdc4ae24fd01ec75955221818bcb7
alexei.matveev <alexei.matveev@gmail.com>
2014-07-31 08:55:23 +0000

    AM: run-ions.scm does 1D and 3D
    Also, from now on the 1D KH calculations use solvent susceptibility
    obtained from a KH pure solvent run. This affectes the numbers
    slightly.

--- test/run-ions.scm
+++ test/run-ions.scm
@@ -7,8 +7,8 @@
 ;;;  ../bgy3d -L ../ -s ./run-ions.scm
 ;;;
 (use-modules
- (guile bgy3d)              ; rism-solute
- (guile molecule)           ; find-molecule
+ (guile bgy3d)          ; rism-solute, hnc3d-run-solute, %null-pointer
+ (guile molecule)       ; find-molecule
  (ice-9 pretty-print))
 
 (define *settings*
@@ -28,15 +28,6 @@
 (define *solvent*
   (find-molecule "water, cSPC/E"))
 
-;;; Pure solvent run here:
-(define *solvent-properties*
-  (rism-solvent *solvent* *settings*))
-
-;;; Solvent susceptibility:
-(define *chi*
-  (assoc-ref *solvent-properties* 'susceptibility))
-
-
 (define *names*
   '("Li+, JC08, SPC/E"
     "Na+, JC08, SPC/E"
@@ -51,22 +42,62 @@
 (define *ions*
   (map find-molecule *names*))
 
-(define (run-one solute closure)
-  (let* ((settings (acons 'closure closure *settings*))
-         (results (rism-solute solute *solvent* settings *chi*))
+;;; Solvent susceptibility  for both closures.  Two  pure solvent runs
+;;; here.  Previous version  used  the same  (HNC) susceptibility  for
+;;; doing KH and  HNC ions in water calculations.  The KH numbers were
+;;; thus slightly wrong.
+(define *chi*
+  (map (lambda (closure)
+         (let ((alist (rism-solvent *solvent*
+                                    (env-set 'closure closure *settings*))))
+           (cons closure
+                 (assoc-ref alist 'susceptibility))))
+       (list 'HNC 'KH)))
+
+;;;
+;;; Call sequence for 1D RISM:
+;;;
+(define (run-1d solute closure)
+  (let* ((settings (env-set 'closure closure *settings*))
+         (chi (assoc-ref *chi* closure))
+         (results (rism-solute solute *solvent* settings chi))
          (solute-properties (assoc-ref results 'solute))
          (free-energy (assoc-ref solute-properties 'XXX)))
     free-energy))
 
-(define *hnc* (map (lambda (m) (run-one m 'HNC)) *ions*))
-(define *kh* (map (lambda (m) (run-one m 'KH)) *ions*))
+;;;
+;;; Call sequence for 3D RISM:
+;;;
+(define (run-3d solute closure)
+  ;; 3D cannot handle large dimensions. FIXME: literals here:
+  (let ((settings (env-set 'N 96 (env-set 'L 10.0 (env-set 'closure closure *settings*)))))
+    (let ((alist (hnc3d-run-solute solute
+                                   *solvent*
+                                   settings
+                                   %null-pointer)))
+      (map vec-destroy (assoc-ref alist 'GUV))
+      (bgy3d-pot-destroy (assoc-ref alist 'POTENTIAL))
+      (bgy3d-restart-destroy (assoc-ref alist 'RESTART))
+      ;; Return free energy:
+      (assoc-ref alist 'XXX))))
+
+(define *hnc-1d* (map (lambda (m) (run-1d m 'HNC)) *ions*))
+(define *kh-1d* (map (lambda (m) (run-1d m 'KH)) *ions*))
+(define *hnc-3d* (map (lambda (m) (run-3d m 'HNC)) *ions*))
+(define *kh-3d* (map (lambda (m) (run-3d m 'KH)) *ions*))
 
 ;;; FIXME: tty is polluted with all  kinds of prints, so we also write
 ;;; selected data into a separate file. See ./out/run-ions-out for the
 ;;; reference output.
-(let* ((hnc (map cons *names* *hnc*))
-       (kh (map cons *names* *kh*))
-       (out `((KH ,kh) (HNC ,hnc))))
+(let* ((hnc-1d (map cons *names* *hnc-1d*))
+       (kh-1d (map cons *names* *kh-1d*))
+       (hnc-3d (map cons *names* *hnc-3d*))
+       (kh-3d (map cons *names* *kh-3d*))
+       (out `((KH-1D ,kh-1d)
+              (KH-3D ,kh-3d)
+              (HNC-1D ,hnc-1d)
+              (HNC-3D ,hnc-3d)
+              )))
   (pretty-print/serial out)
   ;;
   ;; One should not be writing to the same file on multiple workers:
