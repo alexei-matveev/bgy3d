fa68f201222fcf4f78a348a41c1790fcea7095eb
alexei.matveev <alexei.matveev@gmail.com>
2014-09-16 08:39:13 +0000

    AM: two more for custom indent

--- .dir-locals.el
+++ .dir-locals.el
@@ -3,4 +3,6 @@
   . ((indent-tabs-mode . nil)
      (eval . (put 'and-let* 'scheme-indent-function 1))
      (eval . (put 'match 'scheme-indent-function 1))
+     (eval . (put 'unless 'scheme-indent-function 1))
+     (eval . (put 'when 'scheme-indent-function 1))
      (eval . (put 'with-fluids 'scheme-indent-function 1)))))
