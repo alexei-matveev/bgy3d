c9a481008b0599dbba3e7ebe4987ed890a5a5015
alexei.matveev <alexei.matveev@gmail.com>
2014-10-01 13:24:41 +0000

    AM: find extrema, plot TS

--- doc/uo2_paper/uo2-II.d/mkfig-pmf-2d.py
+++ doc/uo2_paper/uo2-II.d/mkfig-pmf-2d.py
@@ -1,5 +1,5 @@
-#!/usr/bin/python3
-
+#!/usr/bin/python
+from __future__ import division, print_function
 import sys
 from mpl_toolkits.mplot3d import Axes3D
 from matplotlib import cm
@@ -16,6 +16,15 @@ T = 1 / beta
 def p(path):
     return "../../../test-qm/uranyl/0w/" + path
 
+N = 128
+L = 10.0
+H = 2 * L / N
+# off-by-one on an 2L interval:
+L1 = L * (N - 2) / N
+def C (i):
+    return i * H - L
+assert L1 - C(N - 1) == 0.0
+
 # Parsing text takes lots of time:
 try:
     g3 = np.load (p("g0,g128.npy"))
@@ -23,15 +32,62 @@ except:
     g3 = np.loadtxt (p("g0,g128.txt"))
     np.save (p("g0,g128.npy"), g3)
 
-g3.shape = (128, 128, 128)
+g3.shape = (N, N, N)
+
+ex = []
+def extrema (g):
+    # plt.imshow (g, extent=(-L, L, -L, L)) # cmap=cm.RdBu)
+    # plt.show ()
+    # x = range (N)
+    # y = range (N)
+    x = np.linspace (-L, L1, N)
+    y = np.linspace (-L, L1, N)
+    print (x)
+    # x, y = np.meshgrid (x, y)
+    from scipy.interpolate import RectBivariateSpline as spline
+    from pts.func import NumDiff
+    G = spline (x, y, g)
+    print (g[0, 0], G(-L, -L))
+
+    def f (r):
+        x, y = r
+        return G(x, y)
+
+    f = NumDiff (f)
+    def gn (r):
+        df = f.fprime (r)
+        return np.linalg.norm (df)
+
+    from scipy.optimize import minimize
+
+    def opt (f, x):
+        res = minimize (f, x, options={"disp": True})
+        return res.x
+
+    xp = opt (lambda r: f(r), [0.0, 3.8])
+    print ("x=", xp, "G=", f(xp), "|grad G|=", gn (xp))
+    # ex.append (xp)
+    xm = opt (lambda r: -f(r), [0.0, 2.4])
+    print ("x=", xm, "G=", f(xm), "|grad G|=", gn (xm))
+    # ex.append (xm)
+    # xt = opt (lambda r: gn(r), [1.5, 2.95])
+    # print ("x=", xt, "G=", f(xt), "|grad G|=", gn (xt))
+    # ex.append (xt)
+    xt = opt (lambda r: gn(r), [1.5, 2.8])
+    print ("x=", xt, "G=", f(xt), "|grad G|=", gn (xt))
+    ex.append (xt)
+
+extrema (g3[:, 63, :])
+ex = np.array (ex)
+print (ex)
 
 # Less than a quater of a plane:
 g2 = g3[64:64+32, 63, 64:64+32]
 
 fig = plt.figure()
 ax = fig.gca(projection='3d')
-X = np.linspace(0, 5, 32)
-Y = np.linspace(0, 5, 32)
+X = np.linspace(C(63), C(64+32-1), 32)
+Y = np.linspace(C(63), C(64+32-1), 32)
 X, Y = np.meshgrid(X, Y)
 
 # Z truncated at 5 kcal here:
@@ -43,6 +99,7 @@ surf = ax.plot_surface (X, Y, Z, rstride=1, cstride=1, cmap=cm.coolwarm,
                         shade = True,
                         linewidth=0.25, antialiased=True)
 cset = ax.contour(X, Y, Z, zdir='z', offset=-5, cmap=cm.coolwarm)
+ax.scatter (ex[:, 1], ex[:, 0], zs=-5, c="black")
 
 ax.set_zlim (-5.0, 5.0)
 ax.set_xlabel ('x, A')
