901436fa9faa30ff89c0d3e928aa8a6e5cd2677d
alexei.matveev <alexei.matveev@gmail.com>
2014-12-17 13:07:24 +0000

    AM: use different symbols, legend for profiles

--- doc/uo2_paper/uo2-II.d/mkfig3.py
+++ doc/uo2_paper/uo2-II.d/mkfig3.py
@@ -8,8 +8,9 @@ from pylab import *
 import matplotlib.pyplot  as plt
 from scipy.interpolate import UnivariateSpline as spline
 
-colorH = "#000000"              # black
 colorS = "#c0c0c0"              # silver
+colorH = "#c0c0c0"
+colorQ = "black" # "#000000"              # black
 
 kcal = 0.0433641195867 # eV
 
@@ -18,6 +19,8 @@ soft = [loadtxt(f) for f in ("soft,interp.txt", "soft,optim.txt")]
 hard = [loadtxt(f) for f in ("hard,interp.txt", "hard,optim.txt")]
 d, e0 = loadtxt ("qmrism,optim.txt"), -777423.62550623293672912596
 print ("shape(d)=", shape (d))
+d0 = copy (d[[0, 0]])
+d0[0, 0] = - d0[0, 0]
 d1 = copy (d[1:-2])             # (1.7 -> 0.2)
 d2 = copy (d[1:-1])             # (1.7 -> 0)
 print ("d1=", d1)
@@ -30,34 +33,36 @@ print ("shape(qmrs)=", shape (qmrs))
 print ("d=", qmrs)
 
 def one ():
-    def pp (two, label, color):
+    def pp (two, symbol, label, color):
         interp, optim = two
         r_int = interp[:, 0]
         g_int = interp[:, 3] / kcal
         r_opt = optim[:, 0]
         g_opt = optim[:, 3] / kcal
         plot (r_int, g_int, "--", color=color)
-        plot (r_opt, g_opt, "o", color=color, label=label)
+        plot (r_opt, g_opt, symbol, color=color, label=label)
 
-    pp (soft, label="Soft", color=colorS)
-    pp (hard, label= "Hard", color=colorH)
+    # For markers see http://matplotlib.org/api/markers_api.html
+    pp (soft, "s", label="Soft", color=colorS)
+    pp (hard, "d", label= "Hard", color=colorH)
     def pq ():
         r = qmrs[:, 0]
         e = (qmrs[:, 3] - e0) / kcal
         print ("r=", r)
-        plot (r, e, "o", color="red")
+        plot (r, e, "o", label="QM", color=colorQ)
+        plot (d0[:, 0], (d0[:, 3] - e0) / kcal, "o", color=colorQ)
 
         espline = spline (r, e, s=0.0)
         rfine = arange (min (r), max (r), 0.02)
-        plot (rfine, map (espline, rfine), "--", color="red")
+        plot (rfine, map (espline, rfine), "--", color=colorQ)
 
     pq()
 
 
-figure (figsize=(8, 4))
+figure (figsize=(6, 6))
 one ()
 xlabel ("q, A")
 ylabel ("G(q), kcal/mol")
-legend ()
+legend (loc=8, numpoints=1)
 # show()
 savefig (sys.argv[1], transparent=True, bbox_inches='tight')
