ead5f7f8dfed94aaffc6be9a0421be4f173b5c53
alexei.matveev <alexei.matveev@gmail.com>
2014-12-29 19:30:41 +0000

    AM: 50 iterations with n=64

--- test-qm/uranyl/mmrism.py
+++ test-qm/uranyl/mmrism.py
@@ -486,7 +486,7 @@ def exchange (s):
                 print ("converged=", info["converged"], "in", info["iterations"])
                 return sm
 
-            maxit = [20] * len (ss)
+            maxit = [50] * len (ss)
             ss = array (map (copt, ss, maxit))
             savetxt ("ss.txt", ss)
 
