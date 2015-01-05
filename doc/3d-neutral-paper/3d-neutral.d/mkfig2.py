e586a40e5df119e37b99accc62c0b42d4af7eba7
lib <lib@theochem.tu-muenchen.de>
2015-01-05 13:23:31 +0000

    BL: change lable text, increase space between two plots in fig2

--- doc/3d-neutral-paper/3d-neutral.d/mkfig2.py
+++ doc/3d-neutral-paper/3d-neutral.d/mkfig2.py
@@ -15,14 +15,18 @@ figure(figsize=(4, 2 * 4.25))
 ax1 = subplot(2, 1, 1)
 ax1.set_title('Gas phase', loc="left")
 plt.plot(d[:, 3], d[:, 4], 'rs')
-ax1.set_xlabel('MM')
-ax1.set_ylabel('FED')
+ax1.set_xlabel('Dipole moment of MM solute')
+ax1.set_ylabel('Dipole moment of FED solute')
+plt.text(0.72, 0.75, '(a)')
 
 # QM, SCF-QM vs. PCM
 ax2 = subplot(2, 1, 2)
 ax2.set_title('Aqueous phase', loc="left")
 plt.plot(d[:, 0], d[:, 1], 'rs', label='QM')
-plt.plot(d[:, 0], d[:, 2], 'bo', label='SCF QM')
+plt.plot(d[:, 0], d[:, 2], 'bo', label='SCF-QM')
+
+# more space between subplots
+subplots_adjust(hspace=0.3)
 
 # line fit
 linewidth=2.0
@@ -34,10 +38,12 @@ fit_fn2 = poly1d(fit)
 plt.plot(d[:, 0], fit_fn1(d[:, 0]), 'r--', linewidth=linewidth)
 plt.plot(d[:, 0], fit_fn2(d[:, 0]), 'b--', linewidth=linewidth)
 
-ax2.set_xlabel('PCM')
-ax2.set_ylabel('RISM')
+ax2.set_xlabel('Dipole moment of PCM solute')
+ax2.set_ylabel('Dipole moment of RISM solute')
 ax2.legend (ncol=1, numpoints=1, frameon=False,
 	fontsize=fontsize, loc='upper left')
+plt.text(1.08, 1.12, '(b)')
+
 
 # show()
 savefig(sys.argv[1], transparent=True, bbox_inches='tight')
