aec7ea6d784ecdce6a5881a242e22784b08e0b0f
lib <lib@theochem.tu-muenchen.de>
2015-02-19 16:56:02 +0000

    BL: adapt fig2, transpose MM vs. GP, compare PCM, AQ with GP

--- doc/3d-neutral-paper/3d-neutral.d/mkfig2.py
+++ doc/3d-neutral-paper/3d-neutral.d/mkfig2.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python
+# -*- coding: utf-8 -*-
 
 import sys
 from numpy import loadtxt, array, shape
@@ -7,7 +8,8 @@ import matplotlib.pyplot as plt
 
 fontsize=10
 dmin, dmax = -0.05, 1.2
-xpos, ypos = dmax - 0.12, dmin + 0.08
+xdmax = dmax - 0.4
+xpos, ypos = xdmax - 0.12, dmin + 0.08
 kw = {"linewidth": 1.5,
       "markeredgewidth": 1.5,
       "markersize": 8}
@@ -16,35 +18,37 @@ d = loadtxt("dipole.dat")
 
 figure(figsize=(4, 2 * 4.25))
 
-# MM and GP
+# MM : d[:, 3]
+# GP : d[:, 4]
 ax1 = subplot(2, 1, 1)
 # ax1.set_title('Gas phase', loc="left")
-plt.plot(d[:, 3], d[:, 4], 'o', color="white", **kw)
+plt.plot(d[:, 4], d[:, 3], 'o', color="white", **kw)
 
-xlim ((dmin, dmax))
+xlim ((dmin, xdmax))
 ylim ((dmin, dmax))
 
-ax1.set_xlabel('Dipole (MM), Debye')
-ax1.set_ylabel('Dipole (FED), Debye')
+# ax1.set_xlabel(u"Dipole (GP), eÅ")
+setp (ax1.get_xticklabels(), visible=False)
+ax1.set_ylabel(u"Dipole (MM), eÅ")
 plt.text (xpos, ypos, 'a', fontsize=15)
 
-# QM, SCF-QM vs. PCM
+# AQ, PCM vs. GP
 ax2 = subplot(2, 1, 2)
 
 # more space between subplots
-subplots_adjust(hspace=0.3)
+# subplots_adjust(hspace=0.3)
 
 # Line fit in the background:
-fit = polyfit(d[:, 0], d[:, 1], 1)
+fit = polyfit(d[:, 4], d[:, 0], 1)
 fit_fn1 = poly1d(fit)
 txt1 = "y = %.2fx + %.2f" % tuple (fit)
 
-fit = polyfit(d[:, 0], d[:, 2], 1)
+fit = polyfit(d[:, 4], d[:, 2], 1)
 fit_fn2 = poly1d(fit)
 txt2 = "y = %.2fx + %.2f" % tuple (fit)
 
 # Line fit across the box:
-two = [dmin, dmax]
+two = [dmin, xdmax]
 plt.plot (two, fit_fn1 (two), '-', color="0.75",
           # label=txt1,
           **kw)
@@ -52,17 +56,19 @@ plt.plot (two, fit_fn2 (two), '-', color="0.00",
           # label=txt2,
           **kw)
 
+# PCM: d[:, 0]
+# AQ: d[:, 2]
 # ax2.set_title('Aqueous phase', loc="left")
-plt.plot(d[:, 0], d[:, 1], 's', color="0.75", label="QM, " + txt1,
+plt.plot(d[:, 4], d[:, 0], 's', color="0.75", label="PCM, " + txt1,
          markeredgecolor="0.50", **kw)
-plt.plot(d[:, 0], d[:, 2], 'o', color="1.00", label="SCF-QM, " + txt2,
+plt.plot(d[:, 4], d[:, 2], 'o', color="1.00", label="AQ, " + txt2,
          **kw)
 
-xlim ((dmin, dmax))
+xlim ((dmin, xdmax))
 ylim ((dmin, dmax))
 
-ax2.set_xlabel('Dipole (PCM), Debye')
-ax2.set_ylabel('Dipole (RISM), Debye')
+ax2.set_xlabel(u"Dipole (Gas Phase), eÅ")
+ax2.set_ylabel(u"Dipole (Aqueous Phase), eÅ")
 ax2.legend (ncol=1, numpoints=1, frameon=False,
 	fontsize=fontsize, loc="upper left")
 plt.text (xpos, ypos, 'b', fontsize=15)
