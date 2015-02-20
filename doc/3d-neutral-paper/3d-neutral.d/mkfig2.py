ec5834adffa8a43ab977b8823ac85988532e87dd
alexei.matveev <alexei.matveev@gmail.com>
2015-02-20 12:10:01 +0000

    AM: set aspect ration for fig2 to 1

--- doc/3d-neutral-paper/3d-neutral.d/mkfig2.py
+++ doc/3d-neutral-paper/3d-neutral.d/mkfig2.py
@@ -9,7 +9,6 @@ import matplotlib.pyplot as plt
 fontsize=10
 dmin, dmax = -0.05, 1.2
 xdmax = dmax - 0.4
-xpos, ypos = xdmax - 0.12, dmin + 0.08
 kw = {"linewidth": 1.5,
       "markeredgewidth": 1.5,
       "markersize": 8}
@@ -17,24 +16,23 @@ kw = {"linewidth": 1.5,
 d = loadtxt("dipole.dat")
 
 figure(figsize=(4, 2 * 4.25))
+fig, (ax1, ax2) = subplots (1, 2, sharex=True, sharey=True)
+ax1.set_aspect (1)
+ax2.set_aspect (1)
 
 # MM : d[:, 3]
 # GP : d[:, 4]
-ax1 = subplot(2, 1, 1)
 # ax1.set_title('Gas phase', loc="left")
-plt.plot(d[:, 4], d[:, 3], 'o', color="white", **kw)
+ax1.plot(d[:, 4], d[:, 3], 'o', color="white", **kw)
 
-xlim ((dmin, xdmax))
-ylim ((dmin, dmax))
+#xlim ((dmin, xdmax))
+#ylim ((dmin, dmax))
 
 # ax1.set_xlabel(u"Dipole (GP), eÅ")
-setp (ax1.get_xticklabels(), visible=False)
+# setp (ax1.get_xticklabels(), visible=False)
 ax1.set_ylabel(u"Dipole (MM), eÅ")
-plt.text (xpos, ypos, 'a', fontsize=15)
 
 # AQ, PCM vs. GP
-ax2 = subplot(2, 1, 2)
-
 # more space between subplots
 # subplots_adjust(hspace=0.3)
 
@@ -49,30 +47,36 @@ txt2 = "y = %.2fx + %.3f" % tuple (fit)
 
 # Line fit across the box:
 two = [dmin, xdmax]
-plt.plot (two, fit_fn1 (two), '-', color="0.75",
+ax2.plot (two, fit_fn1 (two), '-', color="0.50",
           # label=txt1,
           **kw)
-plt.plot (two, fit_fn2 (two), '-', color="0.00",
+ax2.plot (two, fit_fn2 (two), '-', color="0.00",
           # label=txt2,
           **kw)
 
 # PCM: d[:, 0]
 # AQ: d[:, 2]
 # ax2.set_title('Aqueous phase', loc="left")
-plt.plot(d[:, 4], d[:, 0], 's', color="0.75", label="PCM, " + txt1,
+ax2.plot(d[:, 4], d[:, 0], 's', color="0.75", label="PCM, " + txt1,
          markeredgecolor="0.50", **kw)
-plt.plot(d[:, 4], d[:, 2], 'o', color="1.00", label="AQ, " + txt2,
+ax2.plot(d[:, 4], d[:, 2], 'o', color="1.00", label="AQ, " + txt2,
          **kw)
 
-xlim ((dmin, xdmax))
-ylim ((dmin, dmax))
+#xlim ((dmin, xdmax))
+#ylim ((dmin, dmax))
 
+ax1.set_xlabel(u"Dipole (Gas Phase), eÅ")
 ax2.set_xlabel(u"Dipole (Gas Phase), eÅ")
 ax2.set_ylabel(u"Dipole (Aqueous Phase), eÅ")
 ax2.legend (ncol=1, numpoints=1, frameon=False,
-	fontsize=fontsize, loc="upper left")
-plt.text (xpos, ypos, 'b', fontsize=15)
+	fontsize=fontsize, loc="lower right")
 
+xmin, xmax = xlim()
+ymin, ymax = ylim()
+ax1.text (xmin + 0.1, ymax - 0.05, 'a', fontsize=15)
+ax2.text (xmin + 0.1, ymax - 0.05, 'b', fontsize=15)
+
+ax1.locator_params (tight=True, nbins=8)
 
 # show()
 savefig(sys.argv[1], transparent=True, bbox_inches='tight')
